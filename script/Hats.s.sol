d9680e94f139900eba2474baeb3fb41e9310c151
spengrah <code@spengrah.xyz>
2022-10-12 17:59:11 -0500

    prep for new deployments

--- script/Hats.s.sol
+++ script/Hats.s.sol
@@ -5,16 +5,19 @@ import "forge-std/Script.sol";
 import "../src/Hats.sol";
 
 contract DeployHats is Script {
-    string public imageURI = "hats-beta3:";
-    string public name = "Hats Protocol - Beta 3"; // increment this each test deployment
+    string public imageURI = "hats-beta4:";
+    string public name = "Hats Protocol - Beta 4"; // increment this each test deployment
 
     function run() external {
-        vm.startBroadcast();
+        uint256 privKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.rememberKey(privKey);
+        vm.startBroadcast(deployer);
 
         Hats hats = new Hats(name, imageURI);
 
         vm.stopBroadcast();
     }
 
-    // forge script script/Hats.s.sol:DeployHats --rpc-url $GC_RPC --private-key $PRIVATE_KEY --verify --etherscan-api-key $GNOSISSCAN_KEY --broadcast
+    // forge script script/Hats.s.sol -f polygon
+    // forge script script/Hats.s.sol -f polygon --broadcast --verify
 }
