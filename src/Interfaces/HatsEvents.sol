d4cbd6df96c26c9629aed55967d0997727781174
spengrah <code@spengrah.xyz>
2023-01-11 14:46:49 -0600

    update copyright entity

--- src/Interfaces/HatsEvents.sol
+++ src/Interfaces/HatsEvents.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-// Copyright (C) 2023 Hats Protocol
+// Copyright (C) 2023 Haberdasher Labs
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
