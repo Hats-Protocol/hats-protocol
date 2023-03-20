c297e51bd7137490d28d91370e050bce202843d4
spengrah <code@spengrah.xyz>
2023-03-20 14:46:43 -0500

    deploy v1

--- script/Hats.s.sol
+++ script/Hats.s.sol
@@ -7,7 +7,7 @@ import { Hats } from "../src/Hats.sol";
 contract DeployHats is Script {
     string public constant baseImageURI = "ipfs://bafybeigcimbqwfajsnhoq7fqnbdllz7kye7cpdy3adj2sob3wku2llu5bi";
 
-    string public constant name = "Hats Protocol v1-beta1"; // increment this each deployment
+    string public constant name = "Hats Protocol v1"; // increment this each deployment
 
     bytes32 internal constant SALT = bytes32(abi.encode(0x4a75)); // ~ H(4) A(a) T(7) S(5)
 
@@ -15,11 +15,20 @@ contract DeployHats is Script {
         uint256 privKey = vm.envUint("PRIVATE_KEY");
 
         address deployer = vm.rememberKey(privKey);
-        console2.log(vm.getNonce(deployer));
+        console2.log("Deployer: ", deployer);
+        console2.log("Deployer Nonce: ", vm.getNonce(deployer));
 
         vm.startBroadcast(deployer);
 
+        // deploy Hats
         Hats hats = new Hats{ salt: SALT }(name, baseImageURI);
+
+        // mint Hats Protocol Governance tophat
+        hats.mintTopHat(
+            0x2D785497c6C8ce3f4cCff4937D321C37e80705E8, // hatsprotocol.eth
+            "Hats Protocol Governance",
+            baseImageURI
+        );
 
         vm.stopBroadcast();
 
@@ -27,12 +36,12 @@ contract DeployHats is Script {
         console2.log("Hats contract: ", address(hats));
     }
 
-    // forge script script/Hats.s.sol:DeployHats -f goerli
-    // forge script script/Hats.s.sol:DeployHats -f goerli --broadcast --verify
+    // forge script script/Hats.s.sol:DeployHats -f ethereum
+    // forge script script/Hats.s.sol:DeployHats -f ethereum --broadcast --verify
 
     // forge script script/Hats.s.sol:DeployHats --rpc-url http://localhost:8545 --broadcast
 
-    // forge verify-contract --chain-id 5 --num-of-optimizations 10000 --watch --constructor-args $(cast abi-encode "constructor(string,string)" "Hats Protocol v1-beta1" "ipfs://bafybeigcimbqwfajsnhoq7fqnbdllz7kye7cpdy3adj2sob3wku2llu5bi") --compiler-version v0.8.17 0x96bD657Fcc04c71B47f896a829E5728415cbcAa1 src/Hats.sol:Hats $ETHERSCAN_KEY
+    // forge verify-contract --chain-id 1 --num-of-optimizations 10000 --watch --constructor-args $(cast abi-encode "constructor(string,string)" "Hats Protocol v1" "ipfs://bafybeigcimbqwfajsnhoq7fqnbdllz7kye7cpdy3adj2sob3wku2llu5bi") --compiler-version v0.8.17 0x850f3384829D7bab6224D141AFeD9A559d745E3D src/Hats.sol:Hats --etherscan-api-key $ETHERSCAN_KEY
 }
 
 contract DeployHatsAndMintTopHat is Script {
