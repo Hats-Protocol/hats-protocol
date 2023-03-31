bf45aecc6a92cdce625ab13bbe567d4388d2f735
spengrah <code@spengrah.xyz>
2023-03-31 10:02:53 -0500

    deploy v1.0

--- script/Hats.s.sol
+++ script/Hats.s.sol
@@ -7,42 +7,35 @@ import { Hats } from "../src/Hats.sol";
 contract DeployHats is Script {
     string public constant baseImageURI = "ipfs://bafybeigcimbqwfajsnhoq7fqnbdllz7kye7cpdy3adj2sob3wku2llu5bi";
 
-    string public constant name = "Hats Protocol v1"; // increment this each deployment
+    string public constant name = "Hats Protocol v1.0"; // increment this each deployment
 
     bytes32 internal constant SALT = bytes32(abi.encode(0x4a75)); // ~ H(4) A(a) T(7) S(5)
 
-    function run() external {
+    function run() external {   
+        // set up deployer
         uint256 privKey = vm.envUint("PRIVATE_KEY");
-
         address deployer = vm.rememberKey(privKey);
+        // log deployer data
         console2.log("Deployer: ", deployer);
         console2.log("Deployer Nonce: ", vm.getNonce(deployer));
 
         vm.startBroadcast(deployer);
 
-        // deploy Hats
+        // deploy Hats to a deterministic address via CREATE2
         Hats hats = new Hats{ salt: SALT }(name, baseImageURI);
 
-        // mint Hats Protocol Governance topHat
-        // Note: This topHat is not connected to any protocol authorities. The protocol is fully permissionless and not upgradeable.
-        hats.mintTopHat(
-            0x2D785497c6C8ce3f4cCff4937D321C37e80705E8, // hatsprotocol.eth
-            "Hats Protocol Governance",
-            baseImageURI
-        );
-
         vm.stopBroadcast();
-
+        // log deployment data
         console2.log("Salt: ", vm.toString(SALT));
         console2.log("Hats contract: ", address(hats));
     }
 
-    // forge script script/Hats.s.sol:DeployHats -f ethereum
-    // forge script script/Hats.s.sol:DeployHats -f ethereum --broadcast --verify
+    // forge script script/Hats.s.sol:DeployHats -f mainnet
+    // forge script script/Hats.s.sol:DeployHats -f mainnet --broadcast --verify
 
     // forge script script/Hats.s.sol:DeployHats --rpc-url http://localhost:8545 --broadcast
 
-    // forge verify-contract --chain-id 1 --num-of-optimizations 10000 --watch --constructor-args $(cast abi-encode "constructor(string,string)" "Hats Protocol v1" "ipfs://bafybeigcimbqwfajsnhoq7fqnbdllz7kye7cpdy3adj2sob3wku2llu5bi") --compiler-version v0.8.17 0x850f3384829D7bab6224D141AFeD9A559d745E3D src/Hats.sol:Hats --etherscan-api-key $ETHERSCAN_KEY
+    // forge verify-contract --chain-id 1 --num-of-optimizations 10000 --watch --constructor-args $(cast abi-encode "constructor(string,string)" "Hats Protocol v1.0" "ipfs://bafybeigcimbqwfajsnhoq7fqnbdllz7kye7cpdy3adj2sob3wku2llu5bi") --compiler-version v0.8.17 0x9D2dfd6066d5935267291718E8AA16C8Ab729E9d src/Hats.sol:Hats --etherscan-api-key $ETHERSCAN_KEY
 }
 
 contract DeployHatsAndMintTopHat is Script {
